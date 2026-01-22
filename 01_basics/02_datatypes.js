db721f07bf5418789178fd0a3d1c9002165697aa
JAY23kanjariy <jhkanjariya4002@gmail.com>
2026-01-22 12:15:46 +0530

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// 02. datatypes in javascript
+
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3)   // we are using node.js not browser
+
+// number => 2^53
+// bigint 
+// string = ""
+// boolean = true/false
+// null => standalone value 
+// undefined => variable is declare, but value is not assign
+// symbol => unique 
+
+// object => in future.
+
+let name = "jay kanjariya";
+let age = 18;
+let isLoggedIn = false;
+
+// typeof 
+console.log(typeof null);
+console.log(typeof undefined);
