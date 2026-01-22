53b2e6a3a51709b1a2d4e6913441fdeb396d16e0
JAY23kanjariy <jhkanjariya4002@gmail.com>
2026-01-22 13:01:57 +0530

    comparision in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// comparision in js
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true (conversion)
+
+console.log(null > 0);  // true
+console.log(null == 0); // true
+console.log(null >= 0); // false
+
+// console.log(undefined > 0);  // false
+// console.log(undefined == 0); // false
+// console.log(undefined < 0);  // false
