61e336ce7c5fbb8b6b0aa8fe58315adebdec48b4
JAY23kanjariy <jhkanjariya4002@gmail.com>
2026-01-22 12:36:20 +0530

    conversion(casting) of datatypes in js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// 03. Conversion Operations 
+
+let score = "67";
+// let score = true;
+// let score = null;
+// let score = undefined;
+// let score = "67a";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* conversion : Number()/Boolean()/String()/BigInt()
+"33" => 33
+"33a" => NaN
+true =>1/false=>0
+undefined => NaN
+*/
